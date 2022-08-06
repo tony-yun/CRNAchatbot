2239b950e40a91d5213778970fc3d28a822f894f
윤태웅 <yuntaewoong@yuntaeung-ui-MacBookPro.local>
2022-08-06 11:23:21 +0900

    change

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import TabNav from "./navigation/TabNav";
 
 export default function App() {
@@ -11,12 +9,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
