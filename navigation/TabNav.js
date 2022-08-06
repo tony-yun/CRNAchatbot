181cce2c0cd255ff1bbaa1d45f0eaca50bbfb84b
윤태웅 <yuntaewoong@yuntaeung-ui-MacBookPro.local>
2022-08-06 11:12:57 +0900

    navigation setting

---
+++ navigation/TabNav.js
@@ -0,0 +1,16 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const Tab = createBottomTabNavigator();
+
+function TabNav() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default TabNav;
